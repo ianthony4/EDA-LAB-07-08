982d87a1982e42d028d410b74513c29472cb0dc7
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-08-05 17:14:27 -0500

    Clase Phrase - Metodo addword()

--- Src/Phrase.java
+++ Src/Phrase.java
@@ -30,4 +30,37 @@ public class Phrase  {
 	public void setNumbersWord(int numbersWord) {
 		this.numbersWord = numbersWord;
 	}
+
+    // Este metodo nos ayuda a agregar palabras a una frase dada y cada
+	// vez que la frase llegue a 5 palabras quitaremos la primera
+	// palabra y agregue la nueva palabra al final de la oracion
+	
+	public void addword(String word) {
+		word = word.replaceAll("[!?,.:;)(]", "").trim();//Remueve signos de puntuacion
+		if (data == null) {													
+			StringBuilder sb = new StringBuilder();
+			sb.append(word);
+			this.setData(sb);
+			this.numbersWord += 1;
+		} else {
+			StringBuilder temp = this.getData();
+			if (this.numbersWord < max_number_word) {
+				if (temp.length() > 0) {
+					temp.append(" ").append(word);
+
+				} else {
+					temp.append(word);
+				}
+				this.setData(temp);
+				this.numbersWord += 1;
+
+			} else {
+				temp.delete(0, temp.indexOf(" ") + 1);
+				temp.append(" ").append(word);
+				this.setData(temp);
+				this.numbersWord = max_number_word;
+
+			}
+		}
+	}	
 }
