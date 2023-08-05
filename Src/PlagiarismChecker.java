32ef0c2567e184f1547f789e7c74dfae01274eaf
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-08-05 17:16:51 -0500

    Clase PlagiarismChecker - metodo loadFiles()

--- Src/PlagiarismChecker.java
+++ Src/PlagiarismChecker.java
@@ -7,5 +7,30 @@ import javax.swing.SwingUtilities;
 
 public class PlagiarismChecker {
 	
-	
+	static List<String> listDocuments = new ArrayList<String>();
+	static List<AVLTree<String>> trees= new ArrayList<AVLTree<String>>(); //Lista de arboles
+	static String respuesta="";
+
+
+	public PlagiarismChecker() {
+
+	}
+
+    public boolean loadFiles(String[] paths){
+
+		Document d;
+		for(String path: paths){
+			if(listDocuments.contains(path)){
+				continue;
+			}
+			d= new Document(path);
+			trees.add(d.createAVL());
+			listDocuments.add(path);
+			if(trees.get(trees.size()-1)== null){
+				return true;
+			}
+			System.out.println("Se agrego " + path);
+		}
+		return false;
+	}
 }
