efe50cccb0c2b7cff1855ad052e78135bd5ca536
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-08-05 17:15:57 -0500

    Clase PlagiarismChecker - Estructura inical

---
+++ Src/PlagiarismChecker.java
@@ -0,0 +1,11 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.SwingUtilities;
+
+public class PlagiarismChecker {
+	
+	
+}
