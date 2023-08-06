30b6876408b17959a39c6641934ce864c773f9ac
JamesCordova <jcordovas@unsa.edu.pe>
2023-08-06 13:46:00 -0500

    Atributos para clase document

---
+++ Src/Document.java
@@ -0,0 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+/**
+ * Esta clase tendra 3 atributos y contiene una lista
+ * de metodos para ayudarnos a leer y verificar cada
+ * documento que se carguen.
+ */
+
+ public class Document{
+    //Atributos
+	private String fileName;
+	private Phrase phrase;
+	final static int max_number_word=10;
+
+ }
