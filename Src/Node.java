1296c5e43b29b6577d3aecc2223772d617ecfbdb
KevinLlacma <kllacma@unsa.edu.pe>
2023-08-07 18:10:37 -0500

    Se agrega atributos y un constructor de clase node

--- Src/Node.java
+++ Src/Node.java
@@ -2,6 +2,16 @@
 public class Node<T extends Comparable<? super T>> {
 	// Cada nodo tiene una altura, datos y un hijo izquierdo y derecho
 	//Atributos
+	private int height;
+	private Node<T> left, right;
+	private T value;
+	
+	//Getters y Setters  
+	public Node(T value) {
+		this.value = value;
+		height = 1;
+	}
+
 	
 
 }
